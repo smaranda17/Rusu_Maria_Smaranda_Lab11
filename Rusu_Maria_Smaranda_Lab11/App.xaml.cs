2fe1ad27c26cf3395d1c986773d6584cc9d7ef87
Smaranda Maria <rusumariasmaranda@outlook.com>
2020-12-17 19:29:03 +0200

    Add project files.

---
+++ Rusu_Maria_Smaranda_Lab11/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Rusu_Maria_Smaranda_Lab11.Data;
+using System.IO;
+
+namespace Rusu_Maria_Smaranda_Lab11
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Rusu_Maria_Smaranda_Lab11/Models/ShopList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Rusu_Maria_Smaranda_Lab11.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
